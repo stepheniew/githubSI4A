62657566761bd331940748bdd4ba0807f8e5fbd3
stepheniew <161106178+stepheniew@users.noreply.github.com>
2024-03-30 12:50:56 +0700

    php

---
+++ stephen/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo"Hallo, nama saya stephen";
+
+    // ini adalah komentar
+    /*
+    ini juga komentar
+    */
+
+    echo "<br/>";
+    echo date ("Y-m-d, H:i:s");
+    //perhatian :
+    //nama folder untuk menyimpan project di htdocs jangan menggunakan spasi
+    //file harus berextention php
+    //apache harus dalam kondisi start
+    //akses melalui browser dengan alaman https://localhost/namafolder
+    //perhatikan port http yang digunakan, misal 5000, 8000, 8080
+    //gunakan titik 2 ":" sebagai pemisah host dengan port, contoh http://localhost:8080/namafolder
+?>
